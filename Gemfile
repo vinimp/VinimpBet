2a78639444bfbc42eb7c3c808b5ce4c88b65cdc7
vinimp <vinimp@libero.it>
2017-01-20 16:47:00 +0100

    aggiunto devise con email conforma

---
+++ Gemfile
@@ -0,0 +1,33 @@
+source 'http://rubygems.org'
+ruby '2.1.8'
+
+gem 'rails', '4.2.5.1'
+
+gem 'sqlite3'
+
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0', group: :doc
+
+gem 'materialize-sass'
+
+gem 'devise'
+
+gem 'dotenv-rails', :groups => [:development, :test]
+
+group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug'
+end
+
+group :development do
+  # Access an IRB console on exception pages or by using <%= console %> in views
+  gem 'web-console', '~> 2.0'
+end
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ config/environments/development.rb
@@ -0,0 +1,40 @@
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
+  config.eager_load = false
+
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+
+  config.active_support.deprecation = :log
+
+  config.active_record.migration_error = :page_load
+
+  config.assets.debug = true
+
+  config.assets.digest = true
+
+  config.assets.raise_runtime_errors = true
+
+  
+
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      ENV['SENDMAIL_USERNAME'],
+    password:       ENV['SENDMAIL_PASSWORD'],
+    domain:         ENV['MAIL_HOST'],
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }  
+end
