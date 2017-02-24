2a17056ef88b6b3076a839ecc43716b672cd4e31
vinimp <vinimp@libero.it>
2017-02-24 19:18:29 +0100

    aggiustato deciman in decimal

--- db/migrate/20170220210924_add_puntate_to_eurekas.rb
+++ db/migrate/20170220210924_add_puntate_to_eurekas.rb
@@ -1,6 +1,6 @@
 class AddPuntateToEurekas < ActiveRecord::Migration
   def change
-    add_column :eurekas, :puntata11, :deciman
+    add_column :eurekas, :puntata11, :decimal
     add_column :eurekas, :puntata1x, :decimal
     add_column :eurekas, :puntatax, :decimal
     add_column :eurekas, :puntata2, :decimal
