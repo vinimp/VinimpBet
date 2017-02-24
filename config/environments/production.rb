53a76785bf32cc52d6ad5b4930b9b54a9cec9515
vinimp <vinimp@libero.it>
2017-02-24 20:03:45 +0100

    config mail in environment production 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'vinimpbet.herokuapp.com:80' }
   
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
@@ -91,5 +91,5 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }  
-  
+
 end
