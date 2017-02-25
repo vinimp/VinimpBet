5144f525c70fd79cf8e55de4c4b8dd1ac4441c92
vinimp <vinimp@libero.it>
2017-02-25 16:17:37 +0100

    problemi mail 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
     domain:         ENV['MAIL_HOST'],
     address:       'smtp.gmail.com',
     port:          '587',
-    authentication: :plain,
+    authentication: :login,
     enable_starttls_auto: true
   }  
 
