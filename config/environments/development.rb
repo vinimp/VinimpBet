02a38cc89a40b67bc41b602346aa36e4d8ac7fbb
vinimp <vinimp@libero.it>
2017-02-25 09:24:53 +0100

    modificato environment per problemi mail 5

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
 
   config.assets.raise_runtime_errors = true
 
-  
+  config.action_mailer.raise_delivery_errors = true  
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   
