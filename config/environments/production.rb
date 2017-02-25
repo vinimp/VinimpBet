d9078e340d186314cd41006fb85c49c995cb1c1e
vinimp <vinimp@libero.it>
2017-02-25 08:04:35 +0100

    modificato environment per problemi mail 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name:      ENV['SENDMAIL_USERNAME'],
     password:       ENV['SENDMAIL_PASSWORD'],
-    domain:         ENV['MAIL_HOST'],
+    domain:         ENV['http://vinimpbet.herokuapp.com'],
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
