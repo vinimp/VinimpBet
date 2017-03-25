a04abf69e23027bd2f6ce5551e183a7ffa74d828
vinimp <vinimp@libero.it>
2017-03-25 16:02:48 +0100

    impedito eureka ai non loggati 1

--- app/controllers/eurekas_controller.rb
+++ app/controllers/eurekas_controller.rb
@@ -1,6 +1,6 @@
 class EurekasController < ApplicationController
   before_action :set_eureka, only: [:show, :update, :destroy]
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!, only: [:index, :bilancio_edit]
 
   def bilancio_edit
     #@bilancio = Eureka.first
