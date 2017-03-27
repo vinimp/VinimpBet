2d80b200e81ae888e27c43adb78465b7c997a0c6
vinimp <vinimp@libero.it>
2017-03-27 19:55:43 +0200

    aggiunta rigenera password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,20 +21,19 @@ Rails.application.configure do
 
   config.assets.raise_runtime_errors = true
 
-  config.action_mailer.raise_delivery_errors = true  
+  #config.action_mailer.raise_delivery_errors = true  
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' } 
+  config.action_mailer.delivery_method = :smtp 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name:      ENV['SENDMAIL_USERNAME'],
-    password:       ENV['SENDMAIL_PASSWORD'],
-    domain:         ENV['MAIL_HOST'],
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }  
+  config.action_mailer.default :charset => "utf-8"
+  ActionMailer::Base.smtp_settings = { 
+    :address => "smtp.gmail.com", 
+    :port => 587, 
+    :authentication => :plain, 
+    :domain => 'gmail.com', 
+    :user_name => 'vimimp2@gmail.com', 
+    :password => ENV['SECRET'] 
+  }
 end
